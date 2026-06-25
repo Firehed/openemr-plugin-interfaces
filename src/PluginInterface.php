cf4d23c72461ddc62f47d4e9898435c1fca390c7
Eric Stern <eric@ericstern.com>
2026-06-25 09:51:29 -0700

    Add initial PluginInterface

---
+++ src/PluginInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEMR\Plugin;
+
+interface PluginInterface
+{
+    /**
+     * Returns paths to configuration files that define services for dependency
+     * injection. These are merged into the application's container
+     * configuration when the plugin is active.
+     *
+     * @return string[] Plugin-root relative file paths
+     */
+    public static function getConfigFiles(): array;
+}
