4d00a13039d2a8d8d5245df8e0baab09e3cea5cc
Eric Stern <eric@ericstern.com>
2026-06-25 11:30:27 -0700

    Add initial CLI plugin interface

---
+++ src/CliPluginInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEMR\Plugin;
+
+interface CliPluginInterface extends PluginInterface
+{
+    /**
+     * Provide a list of `doctrine/console` commands that the plugin exposes.
+     * This must return the class names of the commands, not instantiated
+     * objects.
+     *
+     * Commands MUST be registered in the DI container
+     *
+     * Invokble commands (which do not need to extend `Command` nor have an
+     * `execute()` method) are preferred.
+     *
+     * @return class-string[]
+     */
+    public static function getCommandClasses(): array;
+}
